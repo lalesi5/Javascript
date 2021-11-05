9fd5b17f7ecd0b97a05c5d56318d64601bae0cf5
Alex Carrera <a_fcalvarez@hotmail.com>
2021-11-05 16:29:17 -0500

    conceptos

---
+++ Conceptos.js
@@ -0,0 +1,42 @@
+//alert("Hola Mundo");
+
+var nombre = "Alex Carrera";
+var altura = 170;
+
+var concatenar = nombre + altura;
+
+//document.write(nombre);
+//document.write(altura);
+
+var datos = document.getElementById("datos");
+//datos.innerHTML = concatenar;
+
+datos.innerHTML = `
+    <h1> Soy la caja de datos</h1>
+    <h2> Mi nombre es: ${nombre}</h2>
+    <h3> mido: ${altura}</h3>
+`;
+
+if(altura >= 190){
+    datos.innerHTML += '<h1>Eres una persona alta</h1>'
+}else{
+    datos.innerHTML += '<h1>Eres una persona bajita</h1>'
+}
+
+function MuestraMiNombre (nombre, altura){
+    var misDatos = `
+    <h1> Soy la caja de datos</h1>
+    <h2> Mi nombre es: ${nombre}</h2>
+    <h3> mido: ${altura}</h3>
+`;
+
+    return misDatos;
+
+}
+
+function imprimir(){
+    var datos = document.getElementById("datos");
+    datos.innerHTML += MuestraMiNombre("Lales",190);
+}
+
+imprimir();
